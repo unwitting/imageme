60460f73aa08431b0f1287253304f198845f2a5a
Jack Preston <jackprestonuk@gmail.com>
2014-10-29 16:34:03 +0000

    Second iteration, recursive processing

--- imageme.py
+++ imageme.py
@@ -1,51 +1,89 @@
-import os
-import re
-import SimpleHTTPServer
-import SocketServer
-import sys
+import os, re, sys, SimpleHTTPServer, SocketServer
 
-CSS_FILE_NAME = '../imageme.css'
-IMAGE_REGEX = '^.+\.(png|jpg|jpeg|tif|tiff|gif|bmp)$'
-IMAGES_PER_ROW = 3
 INDEX_FILE_NAME = 'imageme.html'
+IMAGE_FILE_REGEX = '^.+\.(png|jpg|jpeg|tif|tiff|gif|bmp)$'
+IMAGES_PER_ROW = 3
 
-def compile_index(dir_path, image_files):
-	index_path = os.path.join(dir_path, INDEX_FILE_NAME)
-	index = open(index_path, 'w')
-	index.write('<!DOCTYPE html>\n')
-	index.write('<head>\n')
-	css_file = open(CSS_FILE_NAME)
-	css = css_file.read()
-	css_file.close()
-	index.write('<style>%s</style>' % css)
-	index.write('</head>\n')
-	index.write('<body><div class="content"><table>\n')
-	row_count = 0
-	for f in image_files:
-		if row_count == 0:
-			index.write('<tr>\n')
-		index.write('<td>\n')
-		index.write('<a href="%s"><img src="%s"></a>\n' % (f, f))
-		index.write('</td>\n')
-		row_count += 1
-		if row_count == IMAGES_PER_ROW:
-			row_count = 0
-			index.write('</tr>\n')
-	index.write('</table></div></body>\n')
-	index.close()
-	print('Index file available at %s' % index_path)
+def _create_index_file(root_dir, location, image_files, dirs):
+	html = [
+		'<!DOCTYPE html>',
+		'<html>',
+		'	<head>',
+		'		<title>imageMe</title>'
+		'		<style>',
+		'			html, body {margin: 0;padding: 0;}',
+		'			.header {text-align: right;}',
+		'			.content {padding: 3em;	padding-left: 4em; padding-right: 4em;}',
+		'			.image {max-width: 100%; border-radius: 0.3em;}',
+		'		</style>',
+		'	</head>',
+		'	<body>',
+		'	<div class="content">',
+		'		<h2 class="header">imageMe: ' + location + ' [' + str(len(image_files)) + ' image(s)]</h2>',
+		'		<hr>'
+	]
+	directories = []
+	if root_dir != location: directories = ['..']
+	directories += dirs
+	for directory in directories:
+		link = directory + '/' + INDEX_FILE_NAME
+		html += [
+			'	<h3 class="header">',
+			'	<a href="' + link + '">' + directory + '</a>',
+			'	</h3>'
+		]
+	table_row_count = 1
+	html += ['<hr>', '<table>']
+	for image_file in image_files:
+		if table_row_count == 1: html.append('<tr>')
+		html += [
+			'	<td>',
+			'	<a href="' + image_file + '">',
+			'		<img class="image" src="' + image_file + '">',
+			'	</a>',
+			'	</td>'
+		]
+		if table_row_count == IMAGES_PER_ROW:
+			table_row_count = 0
+			html.append('</tr>')
+		table_row_count += 1
+	html += ['</tr>', '</table>']
+	html += [
+		'	</div>',
+		'	</body>',
+		'</html>'
+	]
+	index_file_path = _get_index_file_path(location)
+	print('Creating index file %s' % index_file_path)
+	index_file = open(index_file_path, 'w')
+	index_file.write('\n'.join(html))
+	index_file.close()
+	return index_file_path
 
-def get_image_files(path):
-	all_files = sorted(os.listdir(path))
-	print('All files:')
-	for f in all_files: print(' ' + f)
-	image_files = [f for f in all_files if re.match(IMAGE_REGEX, f)]
-	print('Image files:')
-	for f in image_files: print(' ' + f)
-	return image_files
+def _get_index_file_path(location):
+	return os.path.join(location, INDEX_FILE_NAME)
+
+def _get_server_port():
+	port = int(sys.argv[1]) if len(sys.argv) > 2 else 8000
+	return port
+
+def clean_up(paths):
+	print('Cleaning up')
+	for path in paths:
+		print('Removing %s' % path)
+		os.unlink(path)
+
+def create_index_files(root_dir):
+	created_files = []
+	for here, dirs, files in os.walk(root_dir):
+		print('Processing %s' % here)
+		image_files = [f for f in files if re.match(IMAGE_FILE_REGEX, f)]
+		created_files.append(_create_index_file(root_dir, here, image_files, dirs))
+	return created_files
 
 def run_server():
-	port = get_server_port()
+	port = _get_server_port()
+	SocketServer.TCPServer.allow_reuse_address = True
 	server = SocketServer.TCPServer(
 		('', port),
 		SimpleHTTPServer.SimpleHTTPRequestHandler
@@ -57,12 +95,12 @@ def run_server():
 	server.serve_forever()
 	return server
 
-def get_server_port():
-	port = int(sys.argv[1]) if len(sys.argv) > 2 else 8000
-	return port
-
-if __name__ == '__main__':
-	path = '.'
-	image_files = get_image_files(path)
-	compile_index(path, image_files)
+created_files = create_index_files('.')
+try:
 	run_server()
+except KeyboardInterrupt:
+	print('User interrupted, stopping')
+except Exception as e:
+	print(e)
+	print('Unhandled exception in server, stopping')
+clean_up(created_files)
